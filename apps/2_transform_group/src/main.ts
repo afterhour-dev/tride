491a884903f9805f15929f97475bb41f3a32a5ac
afterhour <hillprog@proton.me>
2026-07-27 19:30:44 +0200

    transform group

--- apps/2_transform_group/src/main.ts
+++ apps/2_transform_group/src/main.ts
@@ -17,23 +17,44 @@ async function init() {
 	const material = new THREE.MeshBasicMaterial({
 		color: 0x4c9892,
 		// color: 'purple',
-		wireframe: true,
+		// wireframe: true,
 	});
 
-	const mesh = new THREE.Mesh(boxGeometry, material);
-	mesh.position.z = 1.5;
-	mesh.position.x = -0.5;
-	mesh.position.y = 0.6;
+	// ------ Transformations - group ----------------------
 
-	// ------ Transformations - scale ----------------------
-	// EXPLAIN: what is scale, also is it a Vector3 or Euler or something else?
-	// EXPLAIN: what methods are used and how
-	mesh.scale.setScalar(0.5);
-	mesh.scale.set(2, 1.5); // can omit z if you want
-	mesh.scale.z = 1.2;
+	const group = new THREE.Group();
+	// const group2 = new THREE.Group();
+
+	scene.add(group);
+
+	const meshOne = new THREE.Mesh(boxGeometry, material);
+	const meshTwo = new THREE.Mesh(
+		new THREE.BoxGeometry(0.5, 0.5, 0.5),
+		new THREE.MeshBasicMaterial({
+			color: 0x433024,
+		}),
+	);
+	const meshThree = new THREE.Mesh(
+		boxGeometry,
+		new THREE.MeshBasicMaterial({
+			color: '#853a34',
+		}),
+	);
+
+	group.add(meshOne, meshTwo);
+	// group2.add(meshOne, meshTwo, meshThree);
+
+	meshOne.position.x = -1.5;
+	meshThree.position.x = 1.5;
+
+	group.rotation.x = (45 * Math.PI) / 180;
+	group.scale.set(0.5, 0.5);
+	group.position.y = 0.5;
+	group.position.x = -0.5;
+
+	scene.add(meshThree);
+
 	// --------------------------------------------------------
-
-	scene.add(mesh);
 
 	// 3 - Camera
 	const camera = new THREE.PerspectiveCamera(
