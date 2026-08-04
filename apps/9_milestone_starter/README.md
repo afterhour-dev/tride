ca40c37d256f66b14fcf1dc1923394f72853b7d5
afterhour <hillprog@proton.me>
2026-08-05 01:33:09 +0200

    milestone starter

---
+++ apps/9_milestone_starter/README.md
@@ -0,0 +1,3 @@
+# Milestone starter
+
+Just a starter that is good starting point after learning most of basic stuff rlated to Three.js
---
+++ apps/9_milestone_starter/src/main.ts
@@ -0,0 +1,177 @@
+import * as THREE from 'three/webgpu';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import GUI from 'lil-gui';
+import gsap from 'gsap';
+
+import { getRequiredElement } from './util';
+
+const loadingManager = new THREE.LoadingManager();
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+
+const canvas = getRequiredElement<HTMLCanvasElement>('canvas#tride');
+
+// Gui ---------------------------------------------------
+
+const gui = new GUI({
+	width: 350,
+	title: 'Debugging',
+	closeFolders: true,
+});
+
+const awsomeTweaks = gui.addFolder('Awsome tweaking');
+
+const debugObject = {
+	placeholder: false,
+};
+// awsomeTweaks.close();
+
+window.addEventListener('keydown', (ev) => {
+	if (ev.key === 'h') {
+		gui.show(gui._hidden);
+	}
+});
+// ------------------------------------------------------
+
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
+
+async function init() {
+	const scene = new THREE.Scene();
+
+	// 0 - texture stuff -------------------------
+
+	// -------------------------------------------
+
+	// 1 - Geometries Materials Meshes
+
+	let boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+	const material = new THREE.MeshBasicMaterial({
+		// color: debugObject.color,
+		color: 0x4c9892,
+		wireframe: true,
+	});
+
+	const myMesh = new THREE.Mesh(boxGeometry, material);
+
+	// myMesh.position.x = -1.5;
+	// myMesh.position.z = 1.5;
+
+	scene.add(myMesh);
+
+	// ------------- Tweaks ----------------------------------
+
+	// awsomeTweaks.add(debugObject, 'placeholder', 4);
+
+	// --------------------------------------------------------
+
+	// 3 - Perspective Camera
+	const camera = new THREE.PerspectiveCamera(
+		75,
+		sizes.width / sizes.height,
+		0.1,
+		100,
+	);
+
+	camera.position.z = 3;
+	camera.position.y = 1.5;
+	camera.position.x = 1;
+
+	camera.lookAt(myMesh.position);
+
+	scene.add(camera);
+
+	// 4 - Orbit Controls
+	const orbitControls = new OrbitControls(camera, canvas);
+
+	orbitControls.enableDamping = true;
+	// orbitControls.enabled = false;
+	// orbitControls.update()
+
+	// ------------------------------------------------
+	// 5 - Renderer
+	const renderer = new THREE.WebGPURenderer({ canvas });
+	await renderer.init();
+
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+	// 6 - axes helper
+	const axesHelper = new THREE.AxesHelper(5);
+	axesHelper.setColors('red', 'green', 'blue');
+	scene.add(axesHelper);
+	axesHelper.visible = false;
+
+	awsomeTweaks.add(axesHelper, 'visible').name('show axes');
+
+	// ----------------------------------------------------
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setClearColor(0x000000, 1);
+	renderer.render(scene, camera);
+
+	// --------------------------------------------------------------
+	window.addEventListener('resize', () => {
+		sizes.width = window.innerWidth;
+		sizes.height = window.innerHeight;
+
+		camera.aspect = sizes.width / sizes.height;
+
+		camera.updateProjectionMatrix();
+
+		renderer.setSize(sizes.width, sizes.height);
+
+		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+	});
+	// --------------------------------------------------------------
+
+	window.addEventListener('dblclick', () => {
+		const fullScreenElement =
+			// @ts-expect-error can't find it on document but it is there
+			document.fullscreenElement || document.webkitExitFullscreenExit;
+
+		if (!fullScreenElement) {
+			if (canvas.requestFullscreen) {
+				canvas.requestFullscreen();
+				// @ts-expect-error can't find it on document but it is there
+			} else if (canvas.webkitRequestFullScreen) {
+				// @ts-expect-error can't fid it on document but it is there
+				canvas.webkitRequestFullScreen();
+			}
+		} else {
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
+				// @ts-expect-error can't find it on document but it is there
+			} else if (document.webkitExitFullscreen) {
+				// @ts-expect-error can't find it on document but it is there
+				document.webkitExitFullscreen();
+			}
+		}
+	});
+
+	// --------------------------------------------------------------
+	const timer = new THREE.Timer();
+
+	window.requestAnimationFrame(tick);
+	// ----------------------------------------------------
+
+	function tick(timestamp: number) {
+		timer.update(timestamp);
+
+		// const elapsedTime = timer.getElapsed();
+
+		orbitControls.update();
+
+		// well, cube is in the center and camera looks at the center by default
+		// but let's be consistent since cube is what is important
+		camera.lookAt(myMesh.position);
+		// camera.lookAt(new THREE.Vector3()); // default
+
+		renderer.render(scene, camera);
+
+		window.requestAnimationFrame(tick);
+	}
+}
+
+await init();
