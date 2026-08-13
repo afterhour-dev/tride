539e90e856de10dc5bade83ee3709f59be2b2edc
afterhour <hillprog@proton.me>
2026-08-13 15:10:20 +0200

    directional light

---
+++ apps/12_2_directional_light/README.md
@@ -0,0 +1,16 @@
+# Directional Light
+
+Learning about directional light.
+
+## Namera / Intent
+
+Want to know important and useful things about directional light
+
+## Šta treba objasniti u detalje
+
+- will have a **sun-like** effect as if the sun rays were traveling in parallel
+- arguments in instatiation are color and intensity, but we can set the after instatiation too
+- explain what is the position of this light? Because in my scene it seems that it had direction from above; and when I lowered ambient light intensity to 0 (which also neutralizes it) i could see that directional light is comming from above, especially visible on the sphere where upper part of the sphere is visible but the bottom part isn't
+- we can change the position; I also added position to the gui and found out that default position is (0,1,0) ; so that is the reason the light is above
+- for now difference of the distance is not something we can take in consideration for now (explain me why is this?) and in some later lessons when we get into shadows it will matter
+
---
+++ apps/12_2_directional_light/src/main.ts
@@ -0,0 +1,279 @@
+import * as THREE from 'three/webgpu';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+// import gsap from 'gsap';
+
+import { getRequiredElement } from './util';
+
+// const loadingManager = new THREE.LoadingManager();
+// const textureLoader = new THREE.TextureLoader(loadingManager);
+
+// ---------------------------------------------------------
+const canvas = getRequiredElement<HTMLCanvasElement>('canvas#tride');
+
+// Gui -----------------------------------------------------
+
+const gui = new GUI({
+	width: 350,
+	title: 'Debugging',
+	closeFolders: true,
+});
+
+const awsomeTweaks = gui.addFolder('Awsome tweaking');
+
+const debugObject = {
+	//
+};
+// awsomeTweaks.close();
+
+window.addEventListener('keydown', (ev) => {
+	if (ev.key === 'h') {
+		gui.show(gui._hidden);
+	}
+});
+// ------------------------------------------------------
+
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
+
+async function init() {
+	// 0 - Scene
+	const scene = new THREE.Scene();
+
+	// ------------------------------------------------------
+	// 1.1 - Renderer (first part)
+	const renderer = new THREE.WebGPURenderer({ canvas });
+	await renderer.init();
+
+	// ------------------------------------------------------
+	// 2 - Environment map
+
+	// ------------------------------------------------------
+	// 3 -  texture stuff
+	// colorSpace and stuff
+
+	// ------------------------------------------------------
+	// 4 - Text - font loading, TextGeometry, material, mesh
+
+	// --------------------------------------------------
+	// 5 - Lights
+
+	// const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+	// const ambientLight = new THREE.AmbientLight('#a51c81', 8);
+	const ambientLight = new THREE.AmbientLight();
+	ambientLight.color = new THREE.Color(0xffffff);
+	ambientLight.intensity = 0.5;
+
+	scene.add(ambientLight);
+
+	// EXPLAIN: DirectionalLight
+	// EXPLAIN: range of the intensity
+	const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
+
+	// EXPLAIN: Since default is 0,1,0, I wanted that light emits
+	// from the right side; so we lowered y and increased x
+	// (lower ambien light intensity in gui to see the effect of directional light)
+	directionalLight.position.set(1, 0.25, 0);
+
+	scene.add(directionalLight);
+
+	// -----------------------------------------------------
+	// 6 - Geometries Materials Meshes
+
+	const boxGeometry = new THREE.BoxGeometry(0.75, 0.75, 0.75);
+	const torusGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
+	const sphereGreometry = new THREE.SphereGeometry(0.5, 32, 32);
+	const floorGeometry = new THREE.PlaneGeometry(5, 5);
+
+	const material = new THREE.MeshStandardMaterial();
+
+	material.roughness = 0.4;
+
+	const boxMesh = new THREE.Mesh(boxGeometry, material);
+	const torusMesh = new THREE.Mesh(torusGeometry, material);
+	const sphereMesh = new THREE.Mesh(sphereGreometry, material);
+	const floorMesh = new THREE.Mesh(floorGeometry, material);
+
+	// boxMesh.position.x = 1.5;
+	// boxMesh.position.z = 3;
+	// boxMesh.position.z = 1.5;
+	torusMesh.position.x = 1.5;
+	sphereMesh.position.x = -1.5;
+	floorMesh.rotation.x = -Math.PI / 2;
+	floorMesh.position.y = -0.65;
+
+	scene.add(boxMesh, torusMesh, sphereMesh, floorMesh);
+
+	// ------------- Tweaks ----------------------------------
+	// 7 - gui tweaks
+	// awsomeTweaks.add(material, 'wireframe');
+
+	awsomeTweaks
+		.add(ambientLight, 'intensity')
+		.min(0)
+		.max(1)
+		.step(0.001)
+		.name('ambient light intensity');
+
+	awsomeTweaks
+		.addColor(ambientLight, 'color')
+		.name('ambient light color');
+
+	awsomeTweaks
+		.add({ a: '' }, 'a')
+		.name(
+			'----------------------------------------------------------------------------------------------\n----------------------------------------------------------------------------------------------',
+		)
+		.disable();
+	// EXPLAIN: added directional light properties to the
+	// tweaks
+	awsomeTweaks
+		.add(directionalLight, 'intensity')
+		.min(0)
+		.max(1)
+		.step(0.001)
+		.name('directional light intensity');
+
+	awsomeTweaks
+		.addColor(directionalLight, 'color')
+		.name('directional light color');
+
+	// EXPLAIN: adding directional light position to the gui so I can also find out
+	// what is the defaoult
+	awsomeTweaks
+		.add(directionalLight.position, 'x')
+		.step(0.5)
+		.name('direct light x')
+		.min(-100)
+		.max(100);
+	awsomeTweaks
+		.add(directionalLight.position, 'y')
+		.step(0.5)
+		.name('direct light y')
+		.min(-100)
+		.max(100);
+	awsomeTweaks
+		.add(directionalLight.position, 'z')
+		.step(0.5)
+		.name('direct light z')
+		.min(-100)
+		.max(100);
+	// --------------------------------------------------------
+	// 8 - Camera - Perspective Camera
+	const camera = new THREE.PerspectiveCamera(
+		75,
+		sizes.width / sizes.height,
+		0.1,
+		100,
+	);
+
+	// camera.position.z = 3;
+	// camera.position.y = 1.5;
+	// camera.position.x = 1;
+	camera.position.z = 1;
+	camera.position.y = 1;
+	camera.position.x = 2;
+
+	// camera.lookAt(boxMesh.position);
+
+	scene.add(camera);
+
+	// -----------------------------------------------------
+	// 9 - Orbit Controls
+	const orbitControls = new OrbitControls(camera, canvas);
+
+	orbitControls.enableDamping = true;
+	// orbitControls.enabled = false;
+	// orbitControls.update()
+
+	// ------------------------------------------------
+	// 10 - axes helper
+	const axesHelper = new THREE.AxesHelper(5);
+	axesHelper.setColors('red', 'green', 'blue');
+	scene.add(axesHelper);
+	axesHelper.visible = false;
+
+	awsomeTweaks.add(axesHelper, 'visible').name('show axes');
+
+	awsomeTweaks.open();
+
+	// ----------------------------------------------------
+	// 0.2 - Renderer (second part)
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setClearColor(0x000000, 1);
+	renderer.render(scene, camera);
+
+	// --------------------------------------------------------------
+	window.addEventListener('resize', () => {
+		sizes.width = window.innerWidth;
+		sizes.height = window.innerHeight;
+
+		camera.aspect = sizes.width / sizes.height;
+
+		camera.updateProjectionMatrix();
+
+		renderer.setSize(sizes.width, sizes.height);
+
+		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+	});
+	// --------------------------------------------------------------
+
+	window.addEventListener('dblclick', () => {
+		const fullScreenElement =
+			// @ts-expect-error can't find it on document but it is there
+			document.fullscreenElement || document.webkitExitFullscreenExit;
+
+		if (!fullScreenElement) {
+			if (canvas.requestFullscreen) {
+				canvas.requestFullscreen();
+				// @ts-expect-error can't find it on document but it is there
+			} else if (canvas.webkitRequestFullScreen) {
+				// @ts-expect-error can't fid it on document but it is there
+				canvas.webkitRequestFullScreen();
+			}
+		} else {
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
+				// @ts-expect-error can't find it on document but it is there
+			} else if (document.webkitExitFullscreen) {
+				// @ts-expect-error can't find it on document but it is there
+				document.webkitExitFullscreen();
+			}
+		}
+	});
+
+	// --------------------------------------------------------------
+	const timer = new THREE.Timer();
+
+	// window.requestAnimationFrame(tick);
+	renderer.setAnimationLoop(tick);
+	// ----------------------------------------------------
+
+	function tick(timestamp: number) {
+		timer.update(timestamp);
+
+		const elapsedTime = timer.getElapsed();
+
+		orbitControls.update();
+
+		// camera.lookAt(boxMesh.position);
+		// camera.lookAt(new THREE.Vector3()); // default
+
+		boxMesh.rotation.y = elapsedTime * 0.1;
+		sphereMesh.rotation.y = elapsedTime * 0.1;
+		torusMesh.rotation.y = elapsedTime * 0.1;
+
+		boxMesh.rotation.x = 0.15 * elapsedTime;
+		sphereMesh.rotation.x = 0.15 * elapsedTime;
+		torusMesh.rotation.x = 0.15 * elapsedTime;
+
+		renderer.render(scene, camera);
+
+		// window.requestAnimationFrame(tick);
+	}
+}
+
+await init();
